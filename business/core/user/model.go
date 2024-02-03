a2414f8ccba911429b56fe7d94e5625b2979b26a
duexcoast <conor.ux@gmail.com>
2024-02-02 19:04:12 -0500

    create user data models and user core API

--- business/core/user/model.go
+++ business/core/user/model.go
@@ -19,3 +19,24 @@ type User struct {
 	DateCreated  time.Time
 	DateUpdated  time.Time
 }
+
+// NewUser contains information needed to create a new user.
+type NewUser struct {
+	Name            string
+	Email           mail.Address
+	Roles           []Role
+	Department      string
+	Password        string
+	PasswordConfirm string
+}
+
+// UpdateUser contains information needed to update a user.
+type UpdateUser struct {
+	Name            *string
+	Email           *mail.Address
+	Roles           []Role
+	Department      *string
+	Password        *string
+	PasswordConfirm *string
+	Enabled         *bool
+}
