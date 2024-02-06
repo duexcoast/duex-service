e09be959751b641a8ef7c13d458fb02d5088ee29
duexcoast <conor.ux@gmail.com>
2024-02-06 12:43:41 -0500

    fix mispelled directory name

--- business/data/dbmigrate/dbmigrate.go
+++ business/data/dbmigrate/dbmigrate.go
@@ -11,7 +11,7 @@ import (
 	"github.com/ardanlabs/darwin/v3"
 	"github.com/ardanlabs/darwin/v3/dialects/postgres"
 	"github.com/ardanlabs/darwin/v3/drivers/generic"
-	database "github.com/duexcoast/duex-service/business/sys/database"
+	"github.com/duexcoast/duex-service/business/sys/database"
 	"github.com/jmoiron/sqlx"
 )
 
